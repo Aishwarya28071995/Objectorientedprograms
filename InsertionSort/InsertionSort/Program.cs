f37a6ed0482fb99d8de81459e3d4e981d6e0ef36
Aisharya28071995 <gaishwarya58@gmail.com>
2022-07-02 21:30:32 +0530

    Commiting insertion sort files

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 30, 12, 32 };
+            InsertionSort(array);
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            int n = array.Length;
+
+            for(int i=1;i<n;i++)
+            {
+
+                int temp = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > temp)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+
+                }
+
+                array[j + 1] = temp;
+            }
+
+            for(int i=0;i<array.Length;i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
