8c09ef32c3b6e668013b1c6e6b4ce15319895c50
Aisharya28071995 <gaishwarya58@gmail.com>
2022-07-02 21:19:12 +0530

    Commiting Adding integers files

---
+++ AddingIntegers/AddingIntegers/LinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingIntegers
+{
+    internal class LinkedList
+    {
+        public static Node head;
+        public void add(int data)
+        {
+            Node node = new Node(data);
+            if (node == null)
+            {
+                head = node;
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+                //Console.WriteLine(head.data);
+            }
+
+        }
+
+        public int pop(int data)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+                return 0;
+            }
+            else
+            {
+                Node temp = head;
+                // Console.WriteLine(temp.data);
+                while (temp.next != null)
+                {
+                    if (temp.data==data)
+                    {
+                        Console.WriteLine(temp.data);
+                        return temp.data;
+
+                    }
+                    else
+                    {
+                        LinkedList list = new LinkedList();
+                        list.add(data);
+                    }
+                    temp = temp.next;
+                }
+
+            }
+            return 0;
+        }
+    }
+
+    }
+
+
---
+++ AddingIntegers/AddingIntegers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddingIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.add(10);
+            int data = list.pop(10);
+            Console.WriteLine("poped element is " + data);
+
+        }
+    }
+}
