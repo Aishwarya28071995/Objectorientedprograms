056a8913541fd0a78f4c7cc5fd282b44df1b032e
Aisharya28071995 <gaishwarya58@gmail.com>
2022-07-02 21:43:43 +0530

    Commiting files

---
+++ SimpleBalancedParenthesis/SimpleBalancedParenthesis/LinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBalancedParenthesis
+{
+    internal class LinkedList
+    {
+        public static Node head;
+        public void Add(String data)
+        {
+            Node node = new Node(data);
+            if (node == null)
+            {
+                head = node;
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+                Console.WriteLine(head.data);
+            }
+
+        }
+
+        public void pop(String data)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+                
+            }
+            else
+            {
+                Node temp = head;
+                Node p = head;
+                // Console.WriteLine(temp.next);
+                while (temp.next != null)
+                {
+                    if (temp.data.CompareTo(data) == ')')
+                    {
+                        p = temp.next;
+                        Console.WriteLine(temp.data);
+                        
+
+                    }
+                    temp = temp.next;
+                }
+
+            }
+            
+        }
+    }
+    }
+}
